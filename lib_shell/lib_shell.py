54872f7e4d1ba2f441372b00d782d00c5b9914d6
bitranox <rnowotny1966@gmail.com>
2019-08-26 12:32:05 +0200

    refractor

--- lib_shell/lib_shell.py
+++ lib_shell/lib_shell.py
@@ -77,7 +77,8 @@ def run_shell_ls_command(ls_command: List[str], shell: bool = False, communicate
     >>> response = run_shell_ls_command(['echo', 'test'])
     >>> assert 'test' in response.stdout
 
-    >>> response = run_shell_ls_command(['echo', 'test'], pass_std_out_line_by_line=True)
+    >>> response = run_shell_ls_command(['echo', 'test'], pass_std_out_line_by_line=True)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    te...
     >>> assert 'test' in response.stdout
 
     >>> response = run_shell_ls_command(['ls', '-unknown'], raise_on_returncode_not_zero=False, pass_std_out_line_by_line=True)
