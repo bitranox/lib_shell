365691e269a1988f736adf76e47b80afac9f5cb7
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 # this __init__.py is only meant for local package development
-from .lib_shell import *
+try:
+    from .lib_shell import *
+# this we need for pip install --install-option test
+except ImportError:
+    import lib_shell
--- lib_shell/__init__.py
+++ lib_shell/__init__.py
@@ -1,5 +1,13 @@
+import pathlib
 from .lib_shell import *
 
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
 __title__ = 'lib_shell'
-__version__ = '0.0.1'
+__version__ = get_version()
 __name__ = 'lib_shell'
