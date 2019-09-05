4d9d7f58009be270f2635ff15712b41dd663d70e
bitranox <1uGToNSiYo85pWhQcqFu>
2019-09-05 12:32:03 +0200

    refractor

--- lib_shell/lib_shell.py
+++ lib_shell/lib_shell.py
@@ -2,9 +2,13 @@
 import locale
 import logging
 import os
+import queue
 import subprocess
 import sys
-from typing import List, Optional, Tuple
+import threading
+import time
+from typing import Any, List, Optional, Tuple
+
 
 # OWN
 import lib_detect_encoding
@@ -81,6 +85,11 @@ def run_shell_ls_command(ls_command: List[str], shell: bool = False, communicate
     te...
     >>> assert 'test' in response.stdout
 
+    >>> response = run_shell_ls_command(['ls', '--unknown'], pass_std_out_line_by_line=True, raise_on_returncode_not_zero=False)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    >>> assert '--unknown' in response.stderr
+
+
+
     """
     ls_command = [str(s_command) for s_command in ls_command]
     log_settings_struct = lib_parameter.get_default_if_none(log_settings, default=RunShellCommandLogSettings())
@@ -105,7 +114,6 @@ def run_shell_ls_command(ls_command: List[str], shell: bool = False, communicate
         if pass_std_out_line_by_line:
             # Read data from stdout and stderr and passes it to the caller, until end-of-file is reached. Wait for process to terminate.
             stdout, stderr = pass_stdout_stderr_to_caller(my_process, encoding)
-            pass
         else:
             # Send data to stdin. Read data from stdout and stderr, until end-of-file is reached. Wait for process to terminate.
             stdout, stderr = my_process.communicate()
@@ -137,32 +145,89 @@ def run_shell_ls_command(ls_command: List[str], shell: bool = False, communicate
     return command_response
 
 
+# possible memory leak - processes might (and will) sometimes not close - but will close finally when program ends
+# we might end up with many many open threads
+# it works, but afraid to use it on long running programs - it might explode
+# select is also not an option in windows
 def pass_stdout_stderr_to_caller(process: subprocess.Popen, encoding: str) -> Tuple[bytes, bytes]:
+    l_stdout = list()
+    l_stderr = list()
+    queue_stdout = queue.Queue()
+    queue_stderr = queue.Queue()
+    thread_stdout = threading.Thread(target=enque_output, args=(process.stdout, queue_stdout))
+    thread_stderr = threading.Thread(target=enque_output, args=(process.stderr, queue_stderr))
+    thread_stdout.daemon = True
+    thread_stderr.daemon = True
+    thread_stdout.start()
+    thread_stderr.start()
+
+    while True:
+        poll_queue(queue_stdout, sys.stdout, l_stdout, encoding)
+        poll_queue(queue_stderr, sys.stderr, l_stderr, encoding)
+        if process.poll() is not None:
+            break
+
+    time.sleep(0.1)
+    poll_queue(queue_stdout, sys.stdout, l_stdout, encoding)
+    poll_queue(queue_stderr, sys.stderr, l_stderr, encoding)
+    stdout_complete = b''.join(l_stdout)
+    stderr_complete = b''.join(l_stderr)
+    return stdout_complete, stderr_complete
+
+
+def enque_output(out: Any, message_queue: queue.Queue):
+    while True:
+        msg = out.readline()
+        if msg != b'':
+            message_queue.put(msg)
+        else:
+            break
+    out.close()
+
+
+def poll_queue(msg_queue: queue.Queue, target_pipe: Any, msg_list: List, encoding: str) -> None:
+    try:
+        while True:
+            msg_line = msg_queue.get_nowait()
+            msg_list.append(msg_line)
+            msg_line_decoded = msg_line.decode(encoding)
+            target_pipe.write(msg_line_decoded)
+            if hasattr(target_pipe, 'flush'):
+                target_pipe.flush()
+    except queue.Empty:
+        pass
+
+
+# this sometimes does not work under WINE or Windows, it will stuck at STDERR, because
+# no output on STDERR - on linux no problems so far
+def pass_stdout_stderr_to_caller_old(process: subprocess.Popen, encoding: str) -> Tuple[bytes, bytes]:
     l_stdout = list()
     l_stderr = list()
     stdout_to_read = True
     stderr_to_read = True
 
-    while stdout_to_read and stderr_to_read:
-        if stdout_to_read:
-            stdout_line = process.stdout.readline()
-            if stdout_line == b'' and process.poll() is not None:
+    while stdout_to_read or stderr_to_read:
+        if stdout_to_read and process.poll() is None:
+            stdout_line = process.stdout.readline()     # This blocks until it receives a newline
+            if stdout_line == b'':
                 stdout_to_read = False
             else:
                 l_stdout.append(stdout_line)
                 stdout_line_decoded = stdout_line.decode(encoding)
                 sys.stdout.write(stdout_line_decoded)
-                sys.stdout.flush()
+                if hasattr(sys.stdout, 'flush'):
+                    sys.stdout.flush()
 
-        if stderr_to_read:
-            stderr_line = process.stderr.readline()
-            if stderr_line == b'' and process.poll() is not None:
+        if stderr_to_read and process.poll() is None:
+            stderr_line = process.stderr.readline()     # This blocks until it receives a newline
+            if stderr_line == b'':
                 stderr_to_read = False
             else:
                 l_stderr.append(stderr_line)
                 stderr_line_decoded = stderr_line.decode(encoding)
                 sys.stderr.write(stderr_line_decoded)
-                sys.stderr.flush()
+                if hasattr(sys.stderr, 'flush'):
+                    sys.stderr.flush()
 
     stdout_complete = b''.join(l_stdout)
     stderr_complete = b''.join(l_stderr)
