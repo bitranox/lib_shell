8c56e276ce79d75f5bad25060af4c7ed910cff99
bitranox <rnowotny@rotek.at>
2019-11-19 14:46:36 +0100

    refractor

--- lib_shell/lib_shell_commandline.py
+++ lib_shell/lib_shell_commandline.py
@@ -1,4 +1,5 @@
 # stdlib
+import pathlib
 import subprocess
 from typing import List
 
@@ -57,6 +58,17 @@ def get_l_commandline_from_psutil_process(process: psutil.Process) -> List[str]:
     ...     psutil_process=psutil.Process(process.pid)
     ...     assert get_l_commandline_from_psutil_process(psutil_process) == ['nano', './mäßig böse büßer', './müßige bärtige blödmänner']
     ...     psutil_process.kill()
+    ...     # test with blanks in directory and filename
+    ...     process = subprocess.Popen(['./test test/test test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...     psutil_process=psutil.Process(process.pid)
+    ...     assert get_l_commandline_from_psutil_process(psutil_process) == ['/bin/bash', './test test/test test.sh', './test test/some_parameter', 'p1', 'p2']
+    ...     psutil_process.kill()
+    ...     # test with blanks and quotes in directory and filename
+    ...     process = subprocess.Popen(['./test test/test " test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...     psutil_process=psutil.Process(process.pid)
+    ...     expected = ['/bin/bash', './test test/test " test.sh', './test test/some_parameter', 'p1', 'p2']
+    ...     assert get_l_commandline_from_psutil_process(psutil_process) == expected
+    ...     psutil_process.kill()
     ... elif lib_platform.is_platform_darwin:
     ...     process = subprocess.Popen(['open', '-a', 'TextEdit', './mäßig böse büßer', './müßige bärtige blödmänner'])
     ...     psutil_process=psutil.Process(process.pid)
@@ -68,6 +80,9 @@ def get_l_commandline_from_psutil_process(process: psutil.Process) -> List[str]:
     ...     assert get_l_commandline_from_psutil_process(psutil_process) == ['notepad', './mäßig böse büßer', './müßige bärtige blödmänner']
     ...     psutil_process.kill()
 
+    >>> assert lib_shell_shlex.shlex_split_multi_platform('"/home/user/test test.sh" p1 p2') == ['/home/user/test test.sh', 'p1', 'p2']
+    >>> # assert lib_shell_shlex.shlex_split_multi_platform('"/home/user/test \\\\" test.sh" p1 p2') == ['/home/user/test \\" test.sh', 'p1', 'p2']
+
     """
     if lib_platform.is_platform_linux:
         with open('/proc/{pid}/cmdline'.format(pid=process.pid), mode='r') as proc_commandline:
@@ -78,5 +93,117 @@ def get_l_commandline_from_psutil_process(process: psutil.Process) -> List[str]:
     l_commands = lib_list.ls_strip_elements(l_commands)
     l_commands = lib_list.ls_del_empty_elements(l_commands)
     if len(l_commands) == 1:                                                                # pragma: no cover
-        l_commands = lib_shell_shlex.shlex_split_multi_platform(l_commands[0])              # pragma: no cover
+        s_command = l_commands[0]
+        # for the case the command executable contains blank, it would be interpreted as parameter
+        # for instance "/home/user/test test.sh parameter1 parameter2"
+        if lib_platform.is_platform_linux:
+            s_command = get_quoted_command(s_command, process)
+        l_commands = lib_shell_shlex.shlex_split_multi_platform(s_command)                  # pragma: no cover
     return l_commands
+
+
+def get_quoted_command(s_command: str, process: psutil.Process) -> str:
+    """ for the case the command executable contains blank, it would be interpreted as parameter
+    >>> if lib_platform.is_platform_linux:
+    ...     process = subprocess.Popen(['./test test/test test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...     psutil_process=psutil.Process(process.pid)
+    ...     expected = '"./test test/test test.sh" "./test test/some_parameter" p1 p2'
+    ...     assert get_quoted_command('./test test/test test.sh "./test test/some_parameter" p1 p2', psutil_process) == expected
+    ...     psutil_process.kill()
+    ...     # test with quoting in Filename
+    ...     process = subprocess.Popen(['./test test/test " test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...     psutil_process=psutil.Process(process.pid)
+    ...     expected = '"./test test/test \\\\\\\\" test.sh" "./test test/some_parameter" p1 p2'
+    ...     assert get_quoted_command('./test test/test " test.sh "./test test/some_parameter" p1 p2', psutil_process) == expected
+    ...     psutil_process.kill()
+
+    """
+    if " " not in s_command:
+        quoted_command = s_command
+    else:
+        l_command_variations = get_l_command_variations(s_command)
+        s_executable_file = get_executable_file(l_command_variations, process)
+        s_parameters = s_command.split(s_executable_file, 1)[1]
+        if " " not in s_executable_file:    # if there is no blank in the executable, the lexer will work anyway
+            quoted_command = s_command
+        else:
+            quoted_command = quote_string(s_executable_file) + s_parameters
+    return quoted_command
+
+
+def quote_string(unquoted_string: str) -> str:
+    """
+    >>> assert quote_string('test') == '"test"'
+    >>> assert quote_string('te"st') == '"te\\\\\\\\"st"'
+    >>> assert lib_shell_shlex.shlex_split_multi_platform('/home/user/test\\\\"test.sh') == ['/home/user/test"test.sh']
+    >>> assert lib_shell_shlex.shlex_split_multi_platform('"/home/user/test\\\\"test.sh"') == ['/home/user/test"test.sh']
+    """
+    unquoted_string = unquoted_string.replace('"', '\\\\"')
+    quoted_string = '"' + unquoted_string + '"'
+    return quoted_string
+
+
+def get_is_absolute_path(s_command: str) -> bool:
+    """
+    >>> assert get_is_absolute_path('/home/user/test test/test test.sh')
+    >>> assert not get_is_absolute_path('./test test/test test.sh')
+    >>> assert not get_is_absolute_path('test test/test test.sh')
+    """
+
+    if s_command.startswith('/'):
+        return True
+    else:
+        return False
+
+
+def get_l_command_variations(s_command: str) -> List[str]:
+    """
+    >>> assert get_l_command_variations('a b c') == ['a b c', 'a b', 'a']
+
+    """
+    l_command_variations = list()
+    for n_variation in range(s_command.count(" ") + 1):
+        l_command_variations.append(s_command.rsplit(' ', n_variation)[0])
+    return l_command_variations
+
+
+def get_executable_file(l_command_variations: List[str], process: psutil.Process) -> str:
+    """
+    >>> if lib_platform.is_platform_linux:
+    ...     import unittest
+    ...     import os, importlib, importlib.util
+    ...     save_actual_directory = os.path.curdir
+    ...     # ok for doctest under pycharm:
+    ...     module_directory = str(os.path.dirname(os.path.abspath(importlib.util.find_spec('lib_shell').origin)))
+    ...     # for pytest:
+    ...     if not module_directory.endswith('/lib_shell/lib_shell'):
+    ...         module_directory = module_directory + '/lib_shell'
+    ...     os.chdir(module_directory)
+    ...     try:
+    ...         process = subprocess.Popen(['./test test/test test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...         psutil_process=psutil.Process(process.pid)
+    ...         l_command_variations = get_l_command_variations('./test test/test test.sh "./test test/some_parameter" p1 p2')
+    ...         assert get_executable_file(l_command_variations, psutil_process) == './test test/test test.sh'
+    ...         psutil_process.kill()
+    ...         # test with quoting in Filename
+    ...         process = subprocess.Popen(['./test test/test " test.sh', './test test/some_parameter', 'p1', 'p2'])
+    ...         psutil_process=psutil.Process(process.pid)
+    ...         l_command_variations = get_l_command_variations('./test test/test " test.sh "./test test/some_parameter" p1 p2')
+    ...         assert get_executable_file(l_command_variations, psutil_process) == './test test/test " test.sh'
+    ...         l_command_variations = get_l_command_variations('./test test/not_existing.sh "./test test/some_parameter" p1 p2')
+    ...         unittest.TestCase().assertRaises(RuntimeError, get_executable_file, l_command_variations, psutil_process)
+    ...         psutil_process.kill()
+    ...     finally:
+    ...         os.chdir(save_actual_directory)
+
+    """
+    is_absolute_path = get_is_absolute_path(l_command_variations[0])
+    for command_variation in l_command_variations:
+        if is_absolute_path:
+            if pathlib.Path(command_variation).exists():
+                return command_variation
+        else:
+            executable_path = pathlib.Path(process.cwd()) / command_variation
+            if executable_path.exists():
+                return command_variation
+    raise RuntimeError('can not parse the command line, maybe the executable not present anymore: "{cmdline}"'.format(cmdline=l_command_variations[0]))
